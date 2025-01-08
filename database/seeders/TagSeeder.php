7428c38118cfafea3f6407423a65867bccebe2da
Kid Max <kidmax285@gmail.com>
2025-01-08 22:31:31 +0600

    first commit

---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Tag\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
