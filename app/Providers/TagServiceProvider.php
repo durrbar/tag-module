efc9839590190f0f5ce51508dfd3bda734c68a7d
Kid Max <kidmax285@gmail.com>
2025-01-10 05:41:16 +0600

    added Custom Polymorphic Type

--- app/Providers/TagServiceProvider.php
+++ app/Providers/TagServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Tag\Providers;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Nwidart\Modules\Traits\PathNamespace;
@@ -27,6 +28,10 @@ class TagServiceProvider extends ServiceProvider
         $this->registerConfig();
         $this->registerViews();
         $this->loadMigrationsFrom(module_path($this->name, 'database/migrations'));
+
+        Relation::enforceMorphMap([
+            'tag' => 'Modules\Tag\Models\Tag',
+        ]);
     }
 
     /**
