bdbaafd70a07ddc063fe2b960f7cf8706c2ea095
Kid Max <kidmax285@gmail.com>
2026-04-03 15:41:40 +0600

    refactor(requests): add strict return types to tag requests

--- app/Http/Requests/TagCreateRequest.php
+++ app/Http/Requests/TagCreateRequest.php
@@ -15,7 +15,7 @@ class TagCreateRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -25,7 +25,7 @@ class TagCreateRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'name' => ['required', 'string'],
@@ -43,7 +43,7 @@ class TagCreateRequest extends FormRequest
      *
      * @return array
      */
-    public function messages()
+    public function messages(): array
     {
         return [
             'name.required' => 'Name field is required',
@@ -55,7 +55,7 @@ class TagCreateRequest extends FormRequest
         ];
     }
 
-    public function failedValidation(Validator $validator)
+    public function failedValidation(Validator $validator): void
     {
         throw new HttpResponseException(response()->json($validator->errors(), 422));
     }
--- app/Http/Requests/TagUpdateRequest.php
+++ app/Http/Requests/TagUpdateRequest.php
@@ -15,7 +15,7 @@ class TagUpdateRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -25,7 +25,7 @@ class TagUpdateRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'name' => ['required', 'string', 'max: 255'],
@@ -43,7 +43,7 @@ class TagUpdateRequest extends FormRequest
      *
      * @return array
      */
-    public function messages()
+    public function messages(): array
     {
         return [
             'name.string' => 'Name is not a valid string',
@@ -53,7 +53,7 @@ class TagUpdateRequest extends FormRequest
         ];
     }
 
-    public function failedValidation(Validator $validator)
+    public function failedValidation(Validator $validator): void
     {
         throw new HttpResponseException(response()->json($validator->errors(), 422));
     }
