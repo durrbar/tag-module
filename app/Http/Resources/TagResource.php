9469f8010f3157d310fa20acdd0721cedefc306d
Kid Max <kidmax285@gmail.com>
2026-04-03 12:04:18 +0600

    refactor(resource): type tag toArray signature

--- app/Http/Resources/TagResource.php
+++ app/Http/Resources/TagResource.php
@@ -15,7 +15,7 @@ class TagResource extends Resource
      * @param  Request  $request
      * @return array
      */
-    public function toArray($request)
+    public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
